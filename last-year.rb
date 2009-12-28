8afdfc1b71160deb39c183f3f17a0d073b8a82e8
Pat Nakajima <patnakajima@gmail.com>
2009-12-28 12:38:23 -0500

    Nicer output

--- last-year.rb
+++ last-year.rb
@@ -3,19 +3,20 @@ require 'active_support'
 
 $TWEETS = JSON.parse(File.read('my-tweets.json'))
 
-ONE_YEAR_AGO = 1.year.ago.strftime('%m-%d-%Y')
+ONE_YEAR_AGO = 1.year.ago.strftime('%m/%d/%Y')
 
 class Tweetion
   def self.last_year
     $TWEETS.inject([]) do |res, tweet|
       created_at = DateTime.parse(tweet['created_at'])
-      res << tweet if ONE_YEAR_AGO == created_at.strftime('%m-%d-%Y')
+      res << tweet if ONE_YEAR_AGO == created_at.strftime('%m/%d/%Y')
       res
     end
   end
 end
 
 if __FILE__ == $0
+  puts "TWEETS FROM #{ONE_YEAR_AGO}:"
   last_year = Tweetion.last_year.map { |tweet| "- #{tweet['text']}" }
   if limit = ARGV.delete('--limit') || ARGV.delete('-l')
     puts last_year[0..ARGV.last.to_i]
