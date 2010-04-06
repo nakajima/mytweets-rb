aad3197feb10592f76217ce4d8a600def9948664
Pat Nakajima <patnakajima@gmail.com>
2010-04-05 19:00:43 -0700

    Added random.rb to get a random tweet
    Filters out @ replies and tweets with URLs.

---
+++ random.rb
@@ -0,0 +1,18 @@
+require 'json'
+
+$TWEETS = JSON.parse(File.read('my-tweets.json'))
+
+class Tweetion
+  def self.random
+    tweets = $TWEETS
+    tweets = tweets.reject { |tweet| tweet['text'].include?('@') }
+    tweets = tweets.reject { |tweet| tweet['text'].include?('http') }
+    tweets[rand(tweets.size)]
+  end
+end
+
+if __FILE__ == $0
+  tweet = Tweetion.random
+  puts tweet['text']
+  puts "http://twitter.com/#{tweet['user']['screen_name']}/statuses/#{tweet['id']}"
+end
